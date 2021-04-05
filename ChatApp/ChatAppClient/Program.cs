2753e512f06ff44c5754172bd932aec632dcb97f
Connor <61102529+Metater@users.noreply.github.com>
2021-04-05 00:04:28 -0400

    Add working examples for client and server

--- ChatApp/ChatAppClient/Program.cs
+++ ChatApp/ChatAppClient/Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading;
+using MetaMitStandard;
+using MetaMitStandard.Utils;
 
 namespace ChatAppClient
 {
     class Program
     {
+        // Uses MetaMitStandard v0.1.0
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MetaMitClient client = new MetaMitClient();
+
+            IPAddress ip = MetaMitStandard.Utils.ConsoleUtils.ConsoleQuestions.AskIP();
+            ushort port = MetaMitStandard.Utils.ConsoleUtils.ConsoleQuestions.AskPort();
+            string username = MetaMitStandard.Utils.ConsoleUtils.ConsoleQuestions.AskUsername();
+
+            IPEndPoint ep = MetaMitStandard.Utils.NetworkUtils.GetEndPoint(ip, port);
+
+            client.Connected += (object sender, MetaMitStandard.Client.ConnectedEventArgs e) =>
+            {
+                Console.WriteLine("Connected to server!");
+            };
+            client.DataReceived += (object sender, MetaMitStandard.Client.DataReceivedEventArgs e) =>
+            {
+                Console.WriteLine(Encoding.ASCII.GetString(e.data));
+            };
+
+            client.Connect(ep);
+
+            Thread eventPollingThread = new Thread(new ThreadStart(() =>
+            {
+                while (true)
+                {
+                    client.PollEvents();
+                    Thread.Sleep(5);
+                }
+            }));
+            eventPollingThread.Start();
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+                if (!string.IsNullOrEmpty(message) && !string.IsNullOrWhiteSpace(message))
+                {
+                    if (message.ToLower() == "exit")
+                        break;
+                    client.Send(Encoding.ASCII.GetBytes(username + ": " + message));
+                }
+            }
         }
     }
 }
--- ChatApp/ChatAppServer/Program.cs
+++ ChatApp/ChatAppServer/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Text;
+using System.Threading;
+using MetaMitStandard;
+using MetaMitStandard.Server;
 
 namespace ChatAppServer
 {
     class Program
     {
+        // Uses MetaMitStandard v0.1.0
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int listeningPort = 25562;
+            int maxConnectionBacklog = 100;
+            MetaMitServer server = new MetaMitServer(listeningPort, maxConnectionBacklog);
+
+            server.ClientConnected += (object sender, MetaMitStandard.Server.ClientConnectedEventArgs e) =>
+            {
+                Console.WriteLine("Client Connected: \n" + "\tGuid: " + e.guid + "\n" + "\tEndpoint: " + e.ep);
+            };
+            server.DataReceived += (object sender, MetaMitStandard.Server.DataReceivedEventArgs e) =>
+            {
+                server.BroadcastToBut(e.clientConnection, e.data);
+                Console.WriteLine("Client Sent Data: \n" + "\tGuid: " + e.clientConnection.guid + "\n" + "\tData: " + Encoding.ASCII.GetString(e.data));
+            };
+
+            server.Start();
+            Console.WriteLine("Listening on: " + server.ep);
+
+            while (!Console.KeyAvailable)
+            {
+                server.PollEvents();
+                Thread.Sleep(5);
+            }
         }
     }
 }
