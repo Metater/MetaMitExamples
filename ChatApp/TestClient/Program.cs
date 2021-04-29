25b9dab580a09433f84b2f8762cdb240fca50811
Connor <61102529+Metater@users.noreply.github.com>
2021-04-29 00:12:08 -0400

    Add testing stuff

---
+++ ChatApp/TestClient/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading;
+using MetaMitStandard;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MetaMitClient client = new MetaMitClient();
+
+            IPAddress ip = IPAddress.Parse("192.168.1.92");
+            ushort port = 25562;
+
+            IPEndPoint ep = MetaMitStandard.Utils.NetworkUtils.GetEndPoint(ip, port);
+
+            client.Connected += (object sender, MetaMitStandard.Client.ConnectedEventArgs e) =>
+            {
+                Console.WriteLine("Connected to server, Sending data!");
+                for (int i = 0; i < 1; i++)
+                {
+                    byte[] data = new byte[] { 0x08, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x08, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };
+                    client.SendRaw(data);
+                }
+            };
+            client.DataReceived += (object sender, MetaMitStandard.Client.DataReceivedEventArgs e) =>
+            {
+                Console.WriteLine(Encoding.ASCII.GetString(e.data));
+            };
+
+            client.Connect(ep);
+
+            Thread eventPollingThread = new Thread(new ThreadStart(() =>
+            {
+                while (true)
+                {
+                    client.PollEvents();
+                    Thread.Sleep(5);
+                }
+            }));
+            eventPollingThread.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ChatApp/TestServer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using MetaMitStandard;
+
+namespace TestServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int listeningPort = 25562;
+            int maxConnectionBacklog = 100;
+            MetaMitServer server = new MetaMitServer(listeningPort, maxConnectionBacklog);
+            server.ClientConnected += (object sender, MetaMitStandard.Server.ClientConnectedEventArgs e) =>
+            {
+                Console.WriteLine("Client Connected: \n" + "\tGuid: " + e.guid + "\n" + "\tEndpoint: " + e.ep);
+            };
+            server.DataReceived += (object sender, MetaMitStandard.Server.DataReceivedEventArgs e) =>
+            {
+                Console.WriteLine($"Received {e.data.Length} bytes: ");
+                foreach(byte b in e.data)
+                {
+                    Console.WriteLine("\t" + b);
+                }
+            };
+
+            server.Start();
+            Console.WriteLine("Listening on: " + server.ep);
+
+            while (!Console.KeyAvailable)
+            {
+                server.PollEvents();
+                Thread.Sleep(5);
+            }
+        }
+    }
+}
